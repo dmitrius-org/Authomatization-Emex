cff17726c54a047c6dae240277e75dcc27506435
Дмитрий Александров <47624368+dmitrius-org@users.noreply.github.com>
2023-11-12 00:17:06 +0300

    Add files via upload

---
+++ Data-Base/MS-SQL/Tables/tUsers.sql
@@ -0,0 +1,25 @@
+if OBJECT_ID('tUser') is not null
+  drop table tUser
+go
+/* **********************************************************
+tUser - пользователи
+********************************************************** */
+create table tUser
+(
+ UserID            numeric(18,0)  identity --  
+,Brief             nvarchar(512)  not null --
+,Name              nvarchar(512)  null  -- 
+,isBlock           bit      default 0  --
+,DateBlock         datetime       null
+,Login             nvarchar(512) 
+
+,inDatetime        datetime default GetDate()      --
+,updDatetime       datetime default GetDate()      --
+)
+go
+create unique index ao1 on tUser(UserID)
+go
+create unique index ao2 on tUser(Brief)
+go
+grant all on tUser to public
+go
