133c7b0f05d8eba1428a40337917e4173f655b2d
Дмитрий Александров <47624368+dmitrius-org@users.noreply.github.com>
2023-11-13 22:58:47 +0300

    Add files via upload

---
+++ Data-Base/MS-SQL/Procedures/User/create proc GrantSave.sql
@@ -0,0 +1,51 @@
+drop proc if exists GrantSave
+/*
+  GrantSave - сохранение прав пользователя/группы
+  @ObjectID   -- ид объекта
+  @ObjectType -- тп объекта
+              -- 0 - пользователь
+              -- 1 - группа
+
+  на вход подается таблица pGrant
+*/
+go
+create proc GrantSave
+              @ObjectID          numeric(18,0)  
+             ,@ObjectType        int
+as
+  set nocount on
+
+  declare @r int = 0
+
+  delete t
+   from tGrant t (rowlock)
+  inner join pGrant p (nolock)
+          on p.Spid  = @@spid
+         and p.ObjectID = t.ObjectID
+         and p.Value  = 0
+  where t.ObjectType = @ObjectType
+    and t.ObjectID   = @ObjectID
+
+  insert tGrant
+        (ObjectID
+        ,MenuID  
+        ,ObjectType
+        )
+  select p.ObjectID
+        ,p.MenuID   
+        ,@ObjectType
+    from pGrant p with (nolock index=ao1)
+   where p.Spid       = @@spid
+     and p.ObjectID   = @ObjectID
+     and p.Value      = 1
+     and not exists (select 1
+                       from tGrant g with (nolock index=ao1)
+                      where g.ObjectType = @ObjectType
+                        and g.ObjectID   = @ObjectID
+                        and g.MenuID     = p.MenuID
+                     )
+ exit_:
+ return @r
+
+grant exec on GrantSave to public
+go
