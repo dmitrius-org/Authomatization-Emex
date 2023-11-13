112c27c3ac100512c3b5cdc25b46fe191676c80e
Дмитрий Александров <47624368+dmitrius-org@users.noreply.github.com>
2023-11-13 23:00:55 +0300

    Add files via upload

---
+++ Data-Base/MS-SQL/Procedures/Task/create proc TaskUpdate.sql
@@ -0,0 +1,44 @@
+drop proc if exists TaskUpdate
+/*
+  TaskUpdate - 
+*/
+go
+create proc TaskUpdate
+               @TaskID           numeric(18,0) 
+              ,@Brief            nvarchar(60)   --
+              ,@Name	         nvarchar(128)   -- 
+              ,@TaskType         int   -- тип задачи
+              ,@PeriodType       int
+              ,@DateBegin        datetime
+              ,@IsActive         int
+			  ,@Field            nvarchar(max)
+			  ,@LinkID           numeric(18,0)
+			  ,@TimeBegin        Time
+              ,@TimeEnd          Time
+              ,@DayPeriod        int
+              ,@TimePeriod       Time
+as
+  declare @r       int = 0
+		 ,@Type    int
+
+  update [tTask]
+     set [Brief]	   = @Brief
+        ,[Name]		   = @Name
+        ,[TaskType]	   = @TaskType
+        ,[PeriodType]  = @PeriodType
+        ,[DateBegin]   = @DateBegin
+        ,[IsActive]	   = @IsActive
+        ,Field         = @Field
+        ,[updDatetime] = getDate()
+		,LinkID        = @LinkID
+		,TimeBegin 	   = @TimeBegin        
+        ,TimeEnd       = @TimeEnd          
+        ,DayPeriod     = @DayPeriod        
+        ,TimePeriod    = @TimePeriod       
+   where TaskID = @TaskID
+
+ exit_:
+ return @r
+go
+grant exec on TaskUpdate to public
+go
